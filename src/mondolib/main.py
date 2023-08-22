64bc9a1ae61cc3ebf92deadfb865deaeecfae272
Harshad Hegde <hegdehb@gmail.com>
2023-08-22 09:50:28 -0500

    minor updates

---
+++ src/mondolib/main.py
@@ -0,0 +1,18 @@
+"""Main python file."""
+
+
+def demo():
+    """Define API."""
+    print("Hello, World!")
+
+
+def validate():
+    pass
+
+
+def update():
+    pass
+
+
+if __name__ == "__main__":
+    demo()
